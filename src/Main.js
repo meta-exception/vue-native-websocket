29cd520b8e76099d35b9c6257993dc994bcea3cb
Jan Smolko <jankohrasko92@gmail.com>
2018-10-30 14:16:05 +0100

    es5 compatibility

--- src/Main.js
+++ src/Main.js
@@ -29,44 +29,75 @@ export default {
       observer = new Observer(connection, opts)
       Vue.prototype.$socket = observer.WebSocket
     }
-    const hasProxy = typeof Proxy !== 'undefined' && typeof Proxy === 'function' && /native code/.test(Proxy.toString())
+
+    const websocketActions = ['onmessage', 'onclose', 'onerror', 'onopen']
 
     Vue.mixin({
       created () {
         let vm = this
         let sockets = this.$options['sockets']
 
-        if (hasProxy) {
-          this.$options.sockets = new Proxy({}, {
-            set (target, key, value) {
-              Emitter.addListener(key, value, vm)
-              target[key] = value
-              return true
-            },
-            deleteProperty (target, key) {
-              Emitter.removeListener(key, vm.$options.sockets[key], vm)
-              delete target.key
-              return true
+        this.$options.sockets = {
+          _onmessage: [],
+          _onclose: [],
+          _onerror: [],
+          _onopen: [],
+          _onSet: function (type, val) {
+            if (Array.isArray(val)) {
+              val.forEach(() => {
+                this['_' + type].push(val)
+                Emitter.addListener(type, val, vm)
+              })
+            } else {
+              this._onmessage.push(val)
+              Emitter.addListener(type, val, vm)
             }
-          })
-          if (sockets) {
-            Object.keys(sockets).forEach((key) => {
-              this.$options.sockets[key] = sockets[key]
-            })
+          },
+          set onmessage (val) {
+            this._onSet('onmessage', val)
+          },
+          get onmessage () {
+            return this._onmessage
+          },
+          set onclose (val) {
+            this._onSet('onclose', val)
+          },
+          get onclose () {
+            return this._onclose
+          },
+          set onerror (val) {
+            this._onSet('onerror', val)
+          },
+          get onerror () {
+            return this._onerror
+          },
+          set onopen (val) {
+            this._onSet('onopen', val)
+          },
+          get onopen () {
+            return this._onopen
           }
-        } else {
-          Object.seal(this.$options.sockets)
+        }
+
+        if (sockets) {
+          websocketActions.forEach((key) => {
+            this.$options.sockets[key] = sockets[key]
+          })
         }
       },
       beforeDestroy () {
-        if (hasProxy) {
-          let sockets = this.$options['sockets']
+        let sockets = this.$options['sockets']
+        let vm = this
 
-          if (sockets) {
-            Object.keys(sockets).forEach((key) => {
-              delete this.$options.sockets[key]
-            })
-          }
+        if (sockets) {
+          websocketActions.forEach((key) => {
+            if (Array.isArray(this.$options.sockets[key])) {
+              this.$options.sockets[key].forEach((cb) => {
+                Emitter.removeListener(key, cb, vm)
+              })
+              this.$options.sockets[key] = []
+            }
+          })
         }
       }
     })
